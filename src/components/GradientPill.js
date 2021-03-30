c37194be9adf588a616f6170133a2dd9e031b498
ricardo-eth <ricardo.soaresdacosta@gmail.com>
2021-03-30 11:08:55 +0200

    Update

---
+++ src/components/GradientPill.js
@@ -0,0 +1,13 @@
+const GradientPill = ({ colorStart, colorEnd }) => {
+  const linearGradient = `linear-gradient(to right, ${colorStart}, ${colorEnd})`;
+  return (
+    <div
+      className="card-gradient rounded-pill mx-auto mb-4"
+      style={{
+        backgroundImage: linearGradient
+      }}
+    />
+  );
+};
+
+export default GradientPill
